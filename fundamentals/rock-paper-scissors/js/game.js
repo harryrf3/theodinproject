6a94e9473682dae5f78b988ca40c84a3657cacc5
Harry Fahringer III <viccem.dev@gmail.com>
2023-02-22 10:20:10 -0500

    .

--- fundamentals/rock-paper-scissors/js/game.js
+++ fundamentals/rock-paper-scissors/js/game.js
@@ -1,41 +1,60 @@
 // required for user input in console
 const prompt = require('prompt-sync')();
 
+let playerScore = 0;
+let computerScore = 0;
+
 // round conditionals
-function playRound (playerSelection, computerSelection) {
+const playRound  = (playerSelection, computerSelection) => {
   if (computerSelection === playerSelection) {
+    playerScore++;
+    computerScore++;
     return 'Draw!';
   }
   else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    computerScore++;
     return 'Paper beats rock. You Lose!';
   }
   else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    computerScore++;
     return 'Scissors beat paper. You Lose!';
   }
   else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    computerScore++;
     return 'Rock beats scissors. You Lose!';
   }
 
   else if (computerSelection === 'rock' && playerSelection === 'paper') {
+    playerScore++;
     return 'Paper beats rock. You win!';
   }
   else if (computerSelection === 'paper' && playerSelection === 'scissors') {
+    playerScore++;
     return 'Scissors beat paper. You win!';
   }
   else if (computerSelection === 'scissors' && playerSelection === 'rock') {
+    playerScore++;
     return 'Rock beats scissors. You win!';
   }
+  console.log(playerScore, computerScore)
 }
 
 const getComputerChoice = () => {
   const choices = ['rock', 'paper', 'scissors'];
   const randomChoice = Math.floor(Math.random() * choices.length);
-  console.log(choices[randomChoice]);
   return choices[randomChoice];
 };
 
 const getPlayerChoice = () => {
- return prompt("Rock, Paper, or Scissors? ");
+  return prompt("Rock, Paper, or Scissors? ").toLowerCase();
+};
+
+const gameScore = () => {
+  if (playerScore > computerScore) {
+    return `You win with a score of ${playerScore}`;
+  } else {
+    return `The computer wins with a score of ${computerScore}`;
+  }
 };
 
 const game = () => {
@@ -43,6 +62,7 @@ const game = () => {
     const playerSelection = getPlayerChoice();
     const computerSelection = getComputerChoice();
     console.log(playRound(playerSelection, computerSelection));
+    gameScore();
   }
 };
 game();
