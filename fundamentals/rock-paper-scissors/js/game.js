63859305cb54c514daf31f607f68623ebc10a7af
Harry Fahringer III <viccem.dev@gmail.com>
2023-02-22 08:07:12 -0500

    remove broken switch

--- fundamentals/rock-paper-scissors/js/game.js
+++ fundamentals/rock-paper-scissors/js/game.js
@@ -3,68 +3,50 @@ const prompt = require('prompt-sync')();
 
 // round conditionals
 function playRound (playerSelection, computerSelection) {
-
-  // your code here!
-  if (playerSelection == 'rock' && computerSelection == 'paper') {
+  if (computerSelection === playerSelection) {
+    return 'Draw!';
+  }
+  else if (playerSelection == 'rock' && computerSelection == 'paper') {
     return 'Paper beats rock. You Lose!';
   }
-  if (playerSelection == 'paper' && computerSelection == 'scissors') {
+  else if (playerSelection == 'paper' && computerSelection == 'scissors') {
     return 'Scissors beat paper. You Lose!';
   }
-  if (playerSelection == 'scissors' && computerSelection == 'rock') {
+  else if (playerSelection == 'scissors' && computerSelection == 'rock') {
     return 'Rock beats scissors. You Lose!';
   }
 
-  if (computerSelection == 'rock' && playerSelection == 'paper') {
+  else if (computerSelection == 'rock' && playerSelection == 'paper') {
     return 'Paper beats rock. You win!';
   }
-  if (computerSelection == 'paper' && playerSelection == 'scissors') {
+  else if (computerSelection == 'paper' && playerSelection == 'scissors') {
     return 'Scissors beat paper. You win!';
   }
-  if (computerSelection == 'scissors' && playerSelection == 'rock') {
+  else if (computerSelection == 'scissors' && playerSelection == 'rock') {
     return 'Rock beats scissors. You win!';
   }
-  if (computerSelection === computerSelection) {
-    return 'Draw!';
-  }
-
-  // console.log(playerSelection, computerSelection);
 }
-//* working properly
+
 const getComputerChoice = () => {
-  const choice = Math.floor(Math.random() * 3) + 1;
-  switch (choice) {
-    case 1:
-      computer = "rock";
-      break;
-    case 2:
-      computer = "paper";
-      break;
-    case 3:
-      computer = "scissors";
-      break;
-  }
-  return computer;
+  const choices = ['rock', 'paper', 'scissors'];
+  const randomChoice = Math.floor(Math.random() * choices.length);
+  return choices[randomChoice];
 };
-// console.log(getComputerChoice());
 
 const getPlayerChoice = () => {
-  prompt("Rock, Paper, or Scissors? ");
+  prompt("Rock, Paper, or Scissors? ").toLowerCase();
 };
 
-const game = () => {
-  for (let i = 1; i < 5; i++) {
-    getComputerChoice();
-    getPlayerChoice();
+// const game = () => {
+//   for (let i = 1; i <= 5; i++) {
+//     const playerSelection = getPlayerChoice();
+//     const computerSelection = getComputerChoice();
+//     console.log(playRound(playerSelection, computerSelection));
+//   }
+// };
+// game();
 
-    playRound();
-  }
-};
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-game()
 
 
 
