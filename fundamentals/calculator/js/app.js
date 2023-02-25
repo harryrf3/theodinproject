213df39e7ff88e546ec78b1cd97234737b083394
Harry Fahringer III <viccem.dev@gmail.com>
2023-02-24 21:31:29 -0500

    add some query selectors

---
+++ fundamentals/calculator/js/app.js
@@ -0,0 +1,5 @@
+const display = document.querySelector('.display');
+const seven = document.querySelector('.seven')
+const equals = document.querySelector('.equals')
+const plus = document.querySelector('.plus')
+
