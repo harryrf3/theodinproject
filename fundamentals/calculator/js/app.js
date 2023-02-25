a964a613d74606a590e5139ecd2f5ba2404ada46
Harry Fahringer III <viccem.dev@gmail.com>
2023-02-25 10:23:43 -0500

    numbers displaying

--- fundamentals/calculator/js/app.js
+++ fundamentals/calculator/js/app.js
@@ -1,5 +1,52 @@
 const display = document.querySelector('.display');
-const seven = document.querySelector('.seven')
-const equals = document.querySelector('.equals')
-const plus = document.querySelector('.plus')
+const buttons = document.querySelectorAll('.btn');
+const clear = document.querySelector('#clear');
 
+
+let calculation = [];
+
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    if (button.id === 'clear') return;
+    operate(button);
+    updateDisplay(button);
+  });
+});
+
+
+
+const updateDisplay = (button) => {
+  calculation.push(button.textContent);
+  display.textContent = calculation.join('');
+};
+
+
+const clearDisplay = () => {
+  calculation = [];
+};
+
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+
+const subtract = (num1, num2) => {
+  return num1 - num2;
+};
+
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+
+
+const divide = (num1, num2) => {
+  return num1 / num2;
+};
+
+
+const operate = (button) => {
+  console.log(button.textContent);
+};
