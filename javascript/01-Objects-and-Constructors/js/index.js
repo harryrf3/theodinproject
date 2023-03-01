b3c82b775a260293f5ae2304bbea8a73a1261265
Harry Fahringer III <viccem.dev@gmail.com>
2023-03-01 17:51:34 -0500

    .

---
+++ javascript/01-Objects-and-Constructors/js/index.js
@@ -0,0 +1,12 @@
+let library = []
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = false;
+}
+
+function addBookToLibrary() {
+  
+}
