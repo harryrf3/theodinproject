019e1cddf692821397452aaea5628fda0e42649a
Harry Fahringer III <viccem.dev@gmail.com>
2023-03-02 22:10:30 -0500

    add new card to document using javascript

--- javascript/01-Objects-and-Constructors/js/index.js
+++ javascript/01-Objects-and-Constructors/js/index.js
@@ -33,9 +33,11 @@ function addBookToLibrary() {
   library.push(newBook);
 }
 
+
 displayForm.addEventListener('click', () => {
   form.style.display = 'grid'
 })
+
 
 submit.addEventListener('click', (e) => {
   e.preventDefault();
@@ -46,16 +48,32 @@ submit.addEventListener('click', (e) => {
 
 
 function displayBooks() {
-  const p = document.createElement('p')
-  p.textContent = library;
-  // p.textContent = author.value;
-  p.style.color = 'black'
-  document.body.appendChild(p)
-  const div = document.createElement('div')
+  const pTitle = document.createElement('p');
+  const pAuthor = document.createElement('p');
+  const pPages = document.createElement('p');
+  pTitle.textContent = title.value;
+  pAuthor.textContent = author.value; 
+  pPages.textContent = pages.value;
+  const container = document.createElement('div');
+  container.innerHTML = `
+  <div class=card>
+    <p>Title: ${title.value}</p>
+    <p>Author: ${author.value}</p>
+    <p>Pages: ${pages.value}</p>
+  </div>
+  `;
+  // container.append(pTitle, pAuthor, pPages)
+  document.body.append(container)
 }
-
 
 // function removeBookFromLibrary() {
   
 // }
 
+
+
+
+
+
+// You can append to elements that aren't in the DOM yet just like you normally would. 
+// Then, when you're ready to append that element that's in memory to the page- just append it to any element that's already on the page!
