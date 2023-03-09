f7c95609d3e9422d2b354b59db13b6f11319c672
Harry Fahringer III <viccem.dev@gmail.com>
2023-03-09 06:38:49 -0500

    set up base tic tac toe board

--- javascript/tic-tac-toe-project/js/app.js
+++ javascript/tic-tac-toe-project/js/app.js
@@ -1,8 +1,19 @@
-const container = document.querySelector('.container')
+/*
 
+1. Decide which player goes first.
 
-// gameboard frame
-for (let i = 0; i < 9; i++) {
-  const div = document.createElement('div')
-  container.append(div)
-}
+2. On each turn, the player places their symbol ("X" or "O") on an empty square of the grid.
+
+3. After each turn, check if the game is over:
+
+If there are three symbols in a row, horizontally, vertically, or diagonally, the game is over, and the player with the three symbols wins.
+
+If there are no more empty squares on the grid, the game is over, and the result is a tie.
+
+4. If the game is not over, switch to the other player and repeat steps 2-3.
+
+5. If the game is over, display the winner or the tie result.
+
+6. Ask the players if they want to play again. If they do, reset the grid and start over from step 2. If they don't, end the game.
+
+*/
