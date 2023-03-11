526ae8705898ea1b235894c3bb1dea6742c80700
Harry Fahringer III <viccem.dev@gmail.com>
2023-03-11 08:25:19 -0500

    update component to use imported function

--- javascript/webpack-demo/src/index.js
+++ javascript/webpack-demo/src/index.js
@@ -1,10 +1,10 @@
-import _ from 'lodash';
+import myName from './myName.js'
 
 
 function component() {
   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.textContent = myName('Harry');
 
   return element;
 }
