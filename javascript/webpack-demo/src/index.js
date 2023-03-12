ed1918012a1da8d33e527036d54a0ee0bf2f8128
Harry Fahringer III <viccem.dev@gmail.com>
2023-03-12 07:57:58 -0400

    add image to site

--- javascript/webpack-demo/src/index.js
+++ javascript/webpack-demo/src/index.js
@@ -1,11 +1,19 @@
-import myName from './myName.js'
-
+import Icon from './harry-des.jpg';
+import _ from 'lodash';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
-  element.textContent = myName('Harry');
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
+  // Add the image to our exisiting div
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
   return element;
 }
 
