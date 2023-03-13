b9b880b443c311d0d814b848dd889042e0f60dae
Harry Fahringer III <viccem.dev@gmail.com>
2023-03-13 08:24:31 -0400

    add display functions

--- javascript/restaurant-project/src/home.js
+++ javascript/restaurant-project/src/home.js
@@ -1,12 +1,25 @@
-function displayName() {
-  const name = 'Harry';
-  const textNode = document.createTextNode(`Hello, ${name}!`);
-  document.body.appendChild(textNode);
+function displayHomeHeader() {
+  const container = document.querySelector('.container');
+  const header = document.createElement('header');
+  header.innerHTML = '<header class="header"><h3>Taco Bar</h3></header>';
+  container.appendChild(header);
 }
 
-function displayHeader() {
-  const header = document.createElement('div')
-  
+function displayHomeNav() {
+  const nav = ['Home', 'Menu', 'Contact'];
+  const container = document.querySelector('.container');
+  const li = document.createElement('li');
+  li.setAttribute('class', 'sidebar')
+  nav.forEach(item => {
+    const a = document.createElement('a');
+    a.textContent = item;
+    li.appendChild(a);
+  });
+  return container.appendChild(li);
 }
 
-export default displayName;
+// function displayHomeContent() {
+
+// }
+
+export { displayHomeHeader, displayHomeNav };
