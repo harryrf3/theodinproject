5c135c36666a5b44736c82aaf3f1dce2e0eb22b4
Harry Fahringer III <viccem.dev@gmail.com>
2023-03-14 10:43:05 -0400

    move block into container

--- javascript/restaurant-project/src/home.js
+++ javascript/restaurant-project/src/home.js
@@ -22,7 +22,10 @@ function displayHomeContent() {
   const content = document.createElement('main');
   content.setAttribute('class', 'content');
   content.innerHTML = '<h3>The best tacos on the internet!</h3><br/> <h4>You can\'t eat them, but you can stare til your heart\'s content.</h4>';
-  return container.appendChild(content);
+  container.appendChild(content);
+  const card = document.createElement('div');
+  card.setAttribute('class', 'card')
+  return content.appendChild(card);
 }
 
 export { displayHomeHeader, displayHomeNav, displayHomeContent };
