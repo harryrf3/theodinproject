9724ed254c90e4162c237bbabd681d1c66ff8ee7
Harry Fahringer III <viccem.dev@gmail.com>
2023-03-15 08:02:30 -0400

    add images to menu

--- javascript/restaurant-project/src/modules/displayMenu.js
+++ javascript/restaurant-project/src/modules/displayMenu.js
@@ -1,14 +1,28 @@
 const container = document.querySelector('.container');
 
+const imageUrls = [
+  '../src/images/anthony-espinosa-UwdSTMjnN2I-unsplash.jpg',
+  '../src/images/creative-headline-R7FA5a7Itnw-unsplash.jpg',
+  '../src/images/jarett-lopez-6WHl6T-fxU0-unsplash.jpg'
+];
 
 function displayMenu() {
   const card = document.createElement('div');
-  card.setAttribute('class', 'card')
+  card.setAttribute('class', 'card');
   const content = document.createElement('main');
   content.setAttribute('class', 'content');
   card.innerHTML = '<h3>MENU</h4>';
+
+  imageUrls.forEach(url => {
+    const image = new Image();
+    image.src = url;
+    image.addEventListener('load', () => {
+      card.appendChild(image);
+    });
+  });
+
+  content.appendChild(card);
   container.appendChild(content);
-  return content.appendChild(card);
 }
 
 export { displayMenu };
