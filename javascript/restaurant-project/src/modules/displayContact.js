6cc95cf141d0a84df2a93c7697003138baf1ca94
Harry Fahringer III <viccem.dev@gmail.com>
2023-03-15 08:02:44 -0400

    add info to contact

--- javascript/restaurant-project/src/modules/displayContact.js
+++ javascript/restaurant-project/src/modules/displayContact.js
@@ -3,10 +3,23 @@ const container = document.querySelector('.container');
 
 function displayContact() {
   const card = document.createElement('div');
-  card.setAttribute('class', 'card')
+  card.setAttribute('class', 'card');
   const content = document.createElement('main');
   content.setAttribute('class', 'content');
-  card.innerHTML = '<h3>Contact</h4>';
+  card.innerHTML =
+    `
+  <h4>
+    Phone:<br/>
+    (555) 555-1234
+  </h4>
+  <h4>
+    Address:<br/>
+    123 Taco Lane, Taco City, Pa</h4>
+  <h4>
+  Hours:<br/>
+    Mon - Friday: 10am to 10pm<br/>
+    Sat - Sun: 12pm to 10pm
+  </h4>`;
   container.appendChild(content);
   return content.appendChild(card);
 }
