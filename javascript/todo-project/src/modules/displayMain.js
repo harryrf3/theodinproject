ac2988f5cd338034fbcacc14c266428181cd0858
Harry Fahringer III <viccem.dev@gmail.com>
2023-03-18 20:08:56 -0400

    remove test text

--- javascript/todo-project/src/modules/displayMain.js
+++ javascript/todo-project/src/modules/displayMain.js
@@ -3,7 +3,9 @@ const container = document.querySelector('.container')
 function displayMain() {
   const main = document.createElement('main')
   main.setAttribute('class', 'main')
-  main.innerHTML = 'hello from main'
+  const todo = document.createElement('div')
+  todo.setAttribute('class', 'todo')
+  main.appendChild(todo)
   container.appendChild(main)
 }
 
