7134a7b30f6cf28cadd7b966d417caa7c84caa76
Harry Fahringer III <viccem.dev@gmail.com>
2023-03-18 20:09:14 -0400

    create and utilize imports

--- javascript/todo-project/src/index.js
+++ javascript/todo-project/src/index.js
@@ -1,12 +1,13 @@
 console.log('hello from index.js');
 
-import {createTodo} from './modules/createTodo.js';
-import {displayHeader} from './modules/displayHeader.js';
-import './style.css'
+import { createTodo } from './modules/createTodo.js';
+import { displayHeader } from './modules/displayHeader.js';
+import { displayAside } from './modules/displayAside.js';
+import { displayMain } from './modules/displayMain.js';
+import './style.css';
 
 displayHeader();
+displayAside();
+displayMain();
 
-createTodo()
-
-
-console.log(newTodo1);
+createTodo();
