4b6b005aeee6ff52c713fb4b8a5804d443ac40bb
Harry Fahringer III <viccem.dev@gmail.com>
2023-03-19 11:17:08 -0400

    create form

--- javascript/todo-project/src/modules/displayMain.js
+++ javascript/todo-project/src/modules/displayMain.js
@@ -1,12 +1,29 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
 
 function displayMain() {
-  const main = document.createElement('main')
-  main.setAttribute('class', 'main')
-  const todo = document.createElement('div')
-  todo.setAttribute('class', 'todo')
-  main.appendChild(todo)
-  container.appendChild(main)
+  const main = document.createElement('main');
+  main.setAttribute('class', 'main');
+  const todo = document.createElement('div');
+  todo.setAttribute('class', 'todo');
+  todo.innerHTML = `
+  <form>
+    <label for="title">Title:</label>
+    <input type="text" id="title"><br/>
+    <label for="description">Description</label>
+    <input type="text" id="description"><br/>
+    <label for="dueDate">Due Date:</label>
+    <input type="date" id="dueDate"><br/>
+    <label for="priority">Priority:</label>
+    <select id="priority" name="priority">
+      <option value="low">Low</option>
+      <option value="medium">Medium</option>
+      <option value="high">High</option>
+    </select><br/>
+    <button>Submit</button>
+  </form>
+  `;
+  main.appendChild(todo);
+  return container.appendChild(main);
 }
 
-export { displayMain }
+export { displayMain };
